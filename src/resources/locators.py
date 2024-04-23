b525524151b70ab7b005710193b802952f5f9782
renanmarcilio <marcdevrenan@gmail.com>
2024-04-22 23:14:42 -0300

    feat: create locator class

---
+++ src/resources/locators.py
@@ -0,0 +1,11 @@
+class Locator:
+    SEARCH_BUTTON = '//button[@data-element="search-button"]'
+    SEARCH_FIELD = '//input[@data-element="search-form-input"]'
+    SHOW_TOPICS = '//button[@data-toggle-trigger="see-all"]'
+    SORT_BY = '//select[@class="select-input"]'
+    SORT_BY_NEWEST = '//option[@value="1"]'
+    NEWS = 'class:promo-wrapper'
+    ARTICLE_TITLE = 'promo-title'
+    ARTICLE_DESCRIPTION = 'promo-description'
+    ARTICLE_PUBLISH_DATE = 'promo-timestamp'
+    ARTICLE_PICTURE = 'image'
